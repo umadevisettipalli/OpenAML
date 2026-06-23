1a8e7f22fc857d29c3ea510d2a7a0f0c55c0f4eb
umadevisettipalli <sumadevireddy5@gmail.com>
2026-06-23 10:27:50 +0400

    feat: add structuring detector skeleton

---
+++ src/openaml/scenarios/structuring.py
@@ -0,0 +1,16 @@
+"""
+Structuring Detection Scenario
+
+This module will detect customers who perform
+multiple transactions below a reporting threshold
+within a defined time period.
+"""
+
+
+class StructuringDetector:
+    """
+    Placeholder implementation for Version 1.
+    """
+
+    def __init__(self):
+        pass
